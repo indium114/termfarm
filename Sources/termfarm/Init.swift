e792811853c1f0312a41284ba48a14c7c9876f5f
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-01-14 14:29:54 +0200

    Init command

---
+++ Sources/termfarm/Init.swift
@@ -0,0 +1,25 @@
+import ArgumentParser
+import Foundation
+
+struct Init: ParsableCommand {
+    static let configuration = CommandConfiguration(
+        abstract: "Initialise a new Termfarm save file"
+    )
+
+    func run() throws {
+        let farm = FarmState(
+            coins: 100,
+            plots: (0..<3).map { _ in Plot(id: UUID(), plantedCrop: nil, plantedAt: nil) },
+            inventory: Inventory(),
+            market: MarketState(
+                availableSeeds: [ "wheat", "carrot" ],
+                priceModifiers: [:],
+                lastRotation: Date()
+            ),
+            lastUpdated: Date()
+        )
+
+        try saveFarm(farm)
+        print("termfarm save file initialised!") // TODO: Add a nerd font icon
+    }
+}
