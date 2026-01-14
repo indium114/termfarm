3ddd84c91f3642b3ee8baf757cd4e8cf19d9b06b
StikyPiston <156162907+StikyPiston@users.noreply.github.com>
2026-01-14 16:08:47 +0200

    fix: Make CLI actually work

---
+++ Sources/termfarm/TermFarm.swift
@@ -0,0 +1,12 @@
+import ArgumentParser
+
+struct TermFarm: ParsableCommand {
+    static let configuration = CommandConfiguration(
+        commandName: "termfarm",
+        abstract: "A terminal idle farming game",
+        subcommands: [
+            Init.self,
+            Stats.self
+        ]
+    )
+}
--- Sources/termfarm/main.swift
+++ Sources/termfarm/main.swift
@@ -1,11 +1,3 @@
 import ArgumentParser
 
-struct TermFarm: ParsableCommand {
-    static let configuration = CommandConfiguration(
-        commandName: "termfarm",
-        subcommands: [
-            Init.self,
-            Stats.self
-        ]
-    )
-}
+TermFarm.main()
