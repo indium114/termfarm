15c9b647514b11eb2f5682dec846ed33849ae3e7
indium114 <156162907+indium114@users.noreply.github.com>
2026-05-27 19:28:55 +0200

    chore: make basePlotPrice and plotPriceGrowth constant

--- internal/plotpricing.go
+++ internal/plotpricing.go
@@ -2,8 +2,8 @@ package internal
 
 import "math"
 
-var basePlotPrice int = 100
-var plotPriceGrowth float64 = 1.5
+const basePlotPrice int = 100
+const plotPriceGrowth float64 = 1.5
 
 func nextPlotPrice(plotCount int) int {
 	return int(float64(basePlotPrice) * math.Pow(plotPriceGrowth, float64(plotCount-3)))
