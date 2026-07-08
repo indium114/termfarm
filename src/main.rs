111a68bde5aa5884a29fcdd30782c0961a318adf
indium114 <156162907+indium114@users.noreply.github.com>
2026-07-08 12:11:26 +0200

    feat: basic cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,63 @@
+use clap::{Parser, Subcommand};
+
+static VERSION: &str = env!("CARGO_PKG_VERSION");
+
+#[derive(Parser)]
+#[command(name = "termfarm")]
+#[command(version = &VERSION)]
+#[command(about = "A terminal idle farming game", long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Init,
+    Stats,
+    Market,
+    Buy { seed_id: String, amount: u32 },
+    Plant { seed_id: String },
+    Harvest,
+    Inventory,
+    Sell { seed_id: String, amount: u32 },
+    BuyPlot,
+    View,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Init => {
+            todo!()
+        }
+        Commands::Stats => {
+            todo!()
+        }
+        Commands::Market => {
+            todo!()
+        }
+        Commands::Buy { seed_id, amount } => {
+            todo!()
+        }
+        Commands::Plant { seed_id } => {
+            todo!()
+        }
+        Commands::Harvest => {
+            todo!()
+        }
+        Commands::Inventory => {
+            todo!()
+        }
+        Commands::Sell { seed_id, amount } => {
+            todo!()
+        }
+        Commands::BuyPlot => {
+            todo!()
+        }
+        Commands::View => {
+            todo!()
+        }
+    }
 }
