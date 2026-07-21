1d71be61050579cb170030ef43fd25b95694a0c8
indium114 <156162907+indium114@users.noreply.github.com>
2026-07-21 17:33:28 +0200

    chore: bump version

--- flake.nix
+++ flake.nix
@@ -26,7 +26,7 @@
 
         packages.termfarm = pkgs.rustPlatform.buildRustPackage {
           name = "termfarm";
-          version = "2.1.2";
+          version = "2.1.3";
 
           src = ./.;
 
